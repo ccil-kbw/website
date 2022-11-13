28c267f7abc44db0027268b3adc97e609e3e19c2
serafdev <fares@bessrour.com>
2022-11-12 22:21:18 -0500

    Add Iqama
    TODO: Make it beautiful because I live in a terminal and this is only
    practical, ux/ui people where you at?

--- _tabs/iqama.md
+++ _tabs/iqama.md
@@ -8,10 +8,9 @@ To see the Iqama times at the Masjid, [click here](https://iqama.ccil-kbw.com)
 
 # Jumua'a times
 
-- 1ère prière: 12h (Français 12h-12h15, Khotba Arabe 12h15-12h35)
+1ère prière: 12h (Français 12h-12h15, Khotba Arabe 12h15-12h35)
 
-- 2ème prière: 13h (Anglais 13h-13h15, Khotba Arabe 13h15-13h35)
-
+2ème prière: 13h (Anglais 13h-13h15, Khotba Arabe 13h15-13h35)
 
 {% include iqama.html %}
 
--- assets/js/iqama.js
+++ assets/js/iqama.js
@@ -4,22 +4,67 @@ function getJSON(url, callback) {
   xhr.open("GET", url, true)
   xhr.responseType = "json"
   xhr.onload = function() {
-  let status = xhr.status;
-  if (status === 200) {
-  callback(null, xhr.response);
-} else {
-  callback(status, xhr.response);
-}
-};
+    let status = xhr.status;
+    if (status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
   xhr.send()
 }
 
+function appendTimes(key, value) {
+  const prayerName = document.createElement("li")
+  prayerName.textContent = key
+
+  const todaysIqama = document.createElement("li")
+  todaysIqama.textContent = value["iqama"]
+
+  const todaysAdhan = document.createElement("li")
+  todaysAdhan.textContent = value["adhan"]
+
+  const tomorrowsIqamaPlaceHoler = document.createElement("li")
+
+  const ul = document.createElement("ul")
+  ul.appendChild(prayerName)
+  ul.appendChild(todaysIqama)
+  ul.appendChild(todaysAdhan)
+  ul.appendChild(tomorrowsIqamaPlaceHoler)
+  ul.id = key
+  ul.classList = ["prayertimeul"]
+
+  // Append the time container to #ul_times
+  const tableDOM =  document.getElementById("ul_times");
+  tableDOM.appendChild(ul)
+
+}
+
+function appendChangingTime(changingTimes) {
+  for (const [key, value] of Object.entries(changingTimes)) {
+    const tomorrowsIqamaLI = document.getElementById(key).getElementsByTagName("li")[3]
+    tomorrowsIqamaLI.textContent = value
+    tomorrowsIqamaLI.style = "background-color: red;"
+  }
+}
+
 function iqama() {
-  getJSON("https://iqama.ccil-kbw.com/iqamatimes.php", function (err, data) {
+  const url = "http://localhost:9191/"
+  // const url = "https://iqama.ccil-kbw.com/iqamatimes.php"
+  getJSON(url, function(err, data) {
     if (err !== null) {
       console.log("Something went wrong in loading the Iqama.")
     } else {
-      console.log(data)
+      for (const [key, value] of Object.entries(data)) {
+        if (["fajr", "dhuhr", "asr", "maghrib", "isha"].includes(key)) {
+          appendTimes(key, value)
+        }
+        if (key === "salatsToChangeTomorrow") {
+          appendChangingTime(value)
+        }
+      }
     }
   })
 }
+
+iqama()
